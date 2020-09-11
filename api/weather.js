c8257885094e92d7bd29d94d798859e04f2eba60
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-09-11 18:00:47 +0100

    Initial Commit

---
+++ api/weather.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const fetch = require('node-fetch')
+
+const { USER: user, PASS: pass, PORT: port, FROM: from, TO: to } = process.env
+
+const transporter = nodemailer.createTransport({
+  service: 'sendgrid',
+  auth: {
+    user,
+    pass,
+  },
+})
+
+module.exports = async (req, res) => {
+  const weather = await fetch('https://wttr.in/London?format=3').then((body) =>
+    body.text()
+  )
+
+  transporter.sendMail(
+    {
+      subject: 'Serverless Weather Report',
+      text: weather,
+      from,
+      to,
+    },
+    (error) => {
+      if (error) {
+        res.status(500).json({
+          error,
+        })
+      } else {
+        res.status(200).json({
+          message: 'Email Sent',
+        })
+      }
+    }
+  )
+}
